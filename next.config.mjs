279a5c3d191c6fc36f8f2c37d5b7e748f17c6087
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-04-17 12:08:58 +0530

    ♻ Desktop

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,21 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	/**
+	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+	 * out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	}
+	,
+	typescript: {
+		ignoreBuildErrors: true
+	}
 };
 export default config;
